751fb70b71ec67500360df5d44b4b2feae53808f
Chris Priest <nbvfour@gmail.com>
2012-03-20 14:17:44 -0400

    fixed indentation, and added logger code onto the form save function

--- jhouston/forms.py
+++ jhouston/forms.py
@@ -1,3 +1,6 @@
+import logging
+log = logging.getLogger(__name__)
+
 from django.forms import ModelForm
 
 from models import ErrorReport
@@ -7,3 +10,7 @@ class ErrorReportForm(ModelForm):
     class Meta:
         fields = ('message', 'url', 'line_number',)
         model = ErrorReport
+
+    def save(self, *a, **k):
+        log.error('error')
+        return super(ErrorReportForm, self).save(*a, **k)
