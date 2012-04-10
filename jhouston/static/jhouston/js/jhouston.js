95f5d482fffdae0a66e97661cddc8760f94f5cbd
Chris Priest <nbvfour@gmail.com>
2012-04-10 13:25:07 -0400

    added stacktrace lib to repo

--- jhouston/static/jhouston/js/jhouston.js
+++ jhouston/static/jhouston/js/jhouston.js
@@ -1,21 +1,31 @@
 (function() {
   var prevOnError = window.onerror;
+
   window.onerror = function (errorMsg, url, lineNumber) {
-	  if (typeof(jQuery) != 'undefined') {
-  	    jQuery.ajax({
-  	      url: '/jserror/',
-  			  type: 'POST',
-  			  data: {
-  			      message: errorMsg,
-  			      line_number: lineNumber,
-  			      url: url
-  			  }
-  			});
-  	}
-  	if (prevOnError) {
-  	    return prevOnError(errorMsg, url, lineNumber);
-  	}
-  	return false;
+    try {
+      var trace = printStackTrace();
+    } catch(err) {
+      var trace = null;
+    }
+
+    if (typeof(jQuery) !== 'undefined') {
+      jQuery.ajax({
+        url: '/jserror/',
+        type: 'POST',
+        data: {
+          message: errorMsg,
+          line_number: lineNumber,
+          url: url,
+          stack: trace
+        }
+      });
+    }
+
+    if (prevOnError) {
+      return prevOnError(errorMsg, url, lineNumber);
+    }
+
+    return false;
   }
   
   // a logger for logging front end events. extra must be a javascript object
@@ -23,21 +33,21 @@
   // usage:
   // window.jhouston_logger('info', 'this value is wrong', {value: 'wrong'});
   window.jhouston_logger = function(level, message, extra) {
-    extra = JSON.stringify(extra);
-    jQuery.ajax({
-      url: '/jslog/',
-		  type: 'POST',
-		  data: {
-		      message: message,
-		      log_level: level,
-		      extra: extra,
-		  }
-		});
-		
-		// try printing to the console too.
-		try {
-		  console.log(level, message, extra);
-		}
-		catch (error) {}
+    if (typeof(jQuery) !== 'undefined') {
+      jQuery.ajax({
+        url: '/jslog/',
+        type: 'POST',
+        data: {
+          message: message,
+          log_level: level,
+          extra: extra
+        }
+      });
+    }
+
+    // try printing to the console too.
+    try {
+      console.log(level, message, extra);
+    } catch (err) {}
   } 
 })();
